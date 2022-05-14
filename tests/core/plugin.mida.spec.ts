095d2dad5986cb3d79492ead6fe797778bbb2490
Vasile Pește <vasile.peste@protonmail.ch>
2022-05-14 18:08:12 +0200

    Finalize

--- tests/core/plugin.mida.spec.ts
+++
@@ -1,9 +0,0 @@
-import { MidaPlugin } from "@reiryoku/mida";
-
-describe("plugin.mida.ts", (): void => {
-    describe("exports", (): void => {
-        it("a MidaPlugin instance as default", (): void => {
-            expect(require("!/plugin.mida").default).toBeInstanceOf(MidaPlugin);
-        });
-    });
-});
